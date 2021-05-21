7927fe081f905fc791a6ec4609dc7f9a67e5e13c
Kim <1742354607>
2021-05-21 23:32:35 +0800

    更新资讯详情页

--- navigationConstructionTest/app/src/main/java/com/demo/navigationtest/ui/infoboard/ArticleDetailActivity.java
+++ navigationConstructionTest/app/src/main/java/com/demo/navigationtest/ui/infoboard/ArticleDetailActivity.java
@@ -1,31 +1,39 @@
 package com.demo.navigationtest.ui.infoboard;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.demo.navigationtest.R;
-
-import org.sufficientlysecure.htmltextview.HtmlHttpImageGetter;
-import org.sufficientlysecure.htmltextview.HtmlTextView;
+import com.wx.goodview.GoodView;
 
 public class ArticleDetailActivity extends AppCompatActivity {
 
-    //private String htmlContents = null;
+    private GoodView mGoodView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.article_detail);
         this.getSupportActionBar().hide();
-        /*
-        HtmlTextView htmlTextView = findViewById(R.id.article_content);
-        final Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-        htmlContents = bundle.getCharSequence("contents").toString();
-        htmlTextView.setHtml(htmlContents, new HtmlHttpImageGetter(htmlTextView));
-         */
+        mGoodView = new GoodView(this);
+    }
+
+    public void good(View view) {
+        ((ImageView) view).setImageResource(R.drawable.good_checked);
+        mGoodView.setText("+1");
+        mGoodView.show(view);
+    }
+
+    public void bookmark(View view) {
+        ((ImageView) view).setImageResource(R.drawable.bookmark_checked);
+        mGoodView.setTextInfo("收藏成功", Color.parseColor("#ff941A"), 12);
+        mGoodView.show(view);
     }
 
 }

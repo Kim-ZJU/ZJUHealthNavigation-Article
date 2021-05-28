edd1006dead3012454fae02784eaa8898a9a583e
Kim <1742354607>
2021-05-28 23:53:22 +0800

    增加评论功能

--- README.md
+++ README.md
@@ -6,10 +6,24 @@ A2组的医疗资讯模块代码就放在这里啦，方便大家协作和版本
 友情提示：用Android Studio打开此工程，选择Open Existing Project，选择此仓库里的navigationTest文件夹
 
 ## 进度
+
+Kim:
+
 - 2021-5-20
 - 实现资讯界面的资讯列表
 
 - 2021-5-21
 - 实现资讯点击跳转和资讯详情页
 
-- 计划：2021-5-22完善后端，2021-5-23实现前后端连接
+- ~~计划：2021-5-22完善后端，2021-5-23实现前后端连接~~
+- 完善失败，整不透mongoose这玩意儿
+- 参考文档：
+
+node.js操作数据库之MongoDB+mongoose篇 https://www.cnblogs.com/champyin/p/11666892.html  
+Android 连接MongoDB与基本操作 https://blog.csdn.net/u012131769/article/details/40586153  
+Mongoose中文文档 http://mongoosejs.net/docs/index.html  
+
+目前我已经学习到的：
+- 先下载node.js，再下载mongodb，最后用IntelliJ IDEA Ultimate/别的什么软件打开Backend项目，在项目内用 `npm install mongoose` 下载mongoose
+- Mac下载mongodb最后一步新建/data/db时会提示无权限，请参考这个文档 https://blog.csdn.net/cq20110310/article/details/114929072  
+- 理论上IntelliJ IDEA Ultimate会在"mongoose"字段下出现提示要求安装mongoose，点按即可安装，不需要自己打开命令行输入
--- navigationConstructionTest/app/src/main/java/com/demo/navigationtest/ui/infoboard/ArticleDetailActivity.java
+++ navigationConstructionTest/app/src/main/java/com/demo/navigationtest/ui/infoboard/ArticleDetailActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,8 @@ import com.wx.goodview.GoodView;
 public class ArticleDetailActivity extends AppCompatActivity {
     //引用这里的包 https://github.com/venshine/GoodView
     private GoodView mGoodView;
+    private EditText comment_edit_text;
+    private Button comment_btn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +27,17 @@ public class ArticleDetailActivity extends AppCompatActivity {
         setContentView(R.layout.article_detail);
         this.getSupportActionBar().hide();
         mGoodView = new GoodView(this);
+        //TODO：3. 实现评论功能
+        comment_edit_text = findViewById(R.id.comment_edit_text);
+        comment_btn = findViewById(R.id.comment_btn);
+        comment_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ArticleDetailActivity.this, "评论提交成功!等待审核中",
+                        Toast.LENGTH_SHORT).show();
+                comment_edit_text.setText("");
+            }
+        });
     }
     //copy自GoodView提供者的demo，仅实现了点赞效果，没有关联点赞数，也没有再次点击取消点赞的效果
     public void good(View view) {

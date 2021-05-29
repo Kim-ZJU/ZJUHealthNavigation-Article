f26e239aa09b20a7e1a2bd194c679020700b245f
Kim-ZJU <1742354607@qq.com>
2021-05-29 14:46:54 +0800

    增加伪屏蔽功能

--- navigationConstructionTest/app/src/main/java/com/demo/navigationtest/ui/infoboard/ArticleDetailActivity.java
+++ navigationConstructionTest/app/src/main/java/com/demo/navigationtest/ui/infoboard/ArticleDetailActivity.java
@@ -20,10 +20,11 @@ public class ArticleDetailActivity extends AppCompatActivity {
     //引用这里的包 https://github.com/venshine/GoodView
     private GoodView mGoodView;
     private TextView like_num;
-    boolean like_flag = false; //为了实现再次点击取消点赞的效果，需要flag判断是否点击过
+    private boolean like_flag = false; //为了实现再次点击取消点赞的效果，需要flag判断是否点击过
     private EditText comment_edit_text;
     private Button comment_btn;
-    private ImageView article_share;
+    private boolean bookmark_flag = false;
+    private boolean mask_flag = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,10 +43,9 @@ public class ArticleDetailActivity extends AppCompatActivity {
                 comment_edit_text.setText("");
             }
         });
-
-        article_share = findViewById(R.id.article_share);
     }
     //copy自GoodView提供者的demo，仅实现了点赞效果，没有关联点赞数
+    //TODO: 4. 实现点赞功能
     public void good(View view) {
         like_num = findViewById(R.id.article_like_num);
         if (!like_flag) {
@@ -57,19 +57,25 @@ public class ArticleDetailActivity extends AppCompatActivity {
         }
         else {
             ((ImageView) view).setImageResource(R.drawable.good);
-            mGoodView.setText("-1");
             like_num.setText("0");
-            mGoodView.show(view);
             like_flag = false;
         }
     }
-    //copy自GoodView提供者的demo，仅实现了收藏效果，没有关联用户收藏栏，也没有再次点击取消收藏的效果
+    //copy自GoodView提供者的demo，仅实现了收藏效果，没有关联用户收藏栏
+    //TODO: 5. 实现收藏功能
     public void bookmark(View view) {
-        ((ImageView) view).setImageResource(R.drawable.bookmark_checked);
-        mGoodView.setTextInfo("收藏成功", Color.parseColor("#ff941A"), 12);
-        mGoodView.show(view);
+        if (!bookmark_flag) {
+            ((ImageView) view).setImageResource(R.drawable.bookmark_checked);
+            mGoodView.setTextInfo("收藏成功", Color.parseColor("#ff941A"), 12);
+            mGoodView.show(view);
+            bookmark_flag = true;
+        }
+        else {
+            ((ImageView) view).setImageResource(R.drawable.bookmark);
+            bookmark_flag = false;
+        }
     }
-    //TODO: 4. 实现分享功能，可参考"利用 Android 系统原生 API 实现分享功能" https://www.jianshu.com/p/1d4bd2c5ef69
+    //TODO: 6. 实现分享功能，可参考"利用 Android 系统原生 API 实现分享功能" https://www.jianshu.com/p/1d4bd2c5ef69
     public void share(View view) {
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
@@ -78,5 +84,18 @@ public class ArticleDetailActivity extends AppCompatActivity {
         // 指定发送内容的类型
         sendIntent.setType("text/plain");
         startActivity(Intent.createChooser(sendIntent, "分享到..."));
+    }
+    //TODO: 7. 实现屏蔽功能
+    public void mask(View view) {
+        if (!mask_flag) {
+            ((ImageView) view).setImageResource(R.drawable.good_checked);
+            mGoodView.setTextInfo("屏蔽成功", Color.parseColor("#ff941A"), 12);
+            mGoodView.show(view);
+            mask_flag = true;
+        }
+        else {
+            ((ImageView) view).setImageResource(R.drawable.good);
+            mask_flag = false;
+        }
     }
 }
